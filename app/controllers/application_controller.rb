2c14bf3a870bc3baf90b787e14297ae75d3e62aa
Mohamed Saleh <mohamed20163858@gmail.com>
2023-04-02 20:43:17 +0200

    Modify app styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
   end
+
+  def after_sign_in_path_for(_resource)
+    # Replace root_path with the path to your desired page
+    user_path(current_user)
+  end
 end
